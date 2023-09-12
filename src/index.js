31cae8c4019ea9f5b3adfc0583ea2a8f1332ae8c
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2023-09-12 23:23:25 +0300

    corrected markup

--- src/index.js
+++ src/index.js
@@ -10,9 +10,37 @@ let currentPage = 1;
 let perPage = 40;
 
 loadMore.hidden = true;
-
+// console.log('hello')
 formEl.addEventListener('submit', onFopmSubmit);
 loadMore.addEventListener('click', onLoadMore);
+
+function onFopmSubmit(evt) {
+  evt.preventDefault();
+  searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
+  currentPage = 1;
+  cardContainerEl.innerHTML = '';
+  loadMore.hidden = true;
+
+  fetchCardURL(searchQuery, currentPage, perPage)
+    .then(data => {
+      const { hits, totalHits } = data;
+      if (!totalHits || !searchQuery) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+
+      cardContainerEl.insertAdjacentHTML('beforeend', cardMarkupImg(hits));
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+      if (currentPage !== totalHits) {
+        loadMore.hidden = false;
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Sorry! Something went wrong!');
+    });
+}
 
 function onLoadMore() {
   currentPage += 1;
@@ -40,43 +68,3 @@ function onLoadMore() {
       Notiflix.Notify.failure('Sorry! Something went wrong!');
     });
 }
-
-function onFopmSubmit(evt) {
-  evt.preventDefault();
-  searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
-  currentPage = 1;
-  cardContainerEl.innerHTML = '';
-  loadMore.hidden = true;
-
-  fetchCardURL(searchQuery, currentPage, perPage)
-    .then(data => {
-      const { hits, totalHits } = data;
-      if (!totalHits || !searchQuery) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
-
-      cardContainerEl.insertAdjacentHTML('beforeend', cardMarkupImg(hits));
-      Notiflix.Notify.success('Hello! Enjoy your viewing!');
-      if (currentPage !== totalHits) {
-        loadMore.hidden = false;
-      }
-    })
-    .catch(error => {
-      Notiflix.Notify.failure('Sorry! Something went wrong!');
-    });
-}
-// console.log('hello')
-
-// const fetchFriends = async () => {
-//     const token = await fetch("my-api.com/me");
-//     const user = await fetch(`my-api.com/profile?token=${token}`);
-//     const friends = await fetch(`my-api.com/users/${user.id}/friends`);
-//     return friends;
-//   };
-
-//   fetchFriends()
-//     .then(friends => console.log(friends))
-//     .catch(error => console.error(error));
